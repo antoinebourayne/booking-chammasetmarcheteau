8aa63f1033583111d7562c48cf36d493134bacfb
Antoine Bourayne <antoinebourayne@MacBook-Pro-de-Antoine.local>
2025-06-18 13:44:28 +0200

    remove ssl connection

--- server/db.js
+++ server/db.js
@@ -5,6 +5,8 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 5432,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
